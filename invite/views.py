cfcaad54acf373ef48156702d8731655373eae5e
AlyshaJohnson <alysha.johnson@live.co.uk>
2022-11-25 15:12:38 +0000

    add styling to profile page and link party users

--- invite/views.py
+++ invite/views.py
@@ -175,3 +175,20 @@ def delete_song(request, song_id):
     song = get_object_or_404(models.Song, id=song_id)
     song.delete()
     return redirect(get_profile, user_id=request.user.id)
+
+
+@login_required(login_url='/')
+def get_party_profile(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    guest = models.Guest.objects.filter(user_id=user_id).first()
+    wedding = models.Wedding.objects.filter(active=True).first()
+    food = models.Food.objects.all()
+    song = models.Song.objects.filter(guest=guest)
+    context = {
+        'user': user,
+        'guest': guest,
+        'wedding': wedding,
+        'food': food,
+        'song': song,
+    }
+    return render(request, 'invite/profile.html', context)
